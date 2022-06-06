5573758db30a43acd2b78dd5bea184fa371690be
purple-duckling <purple-duckling@protonmail.com>
2022-06-06 20:56:28 +0300

    primitives

--- primitives.go
+++ primitives.go
@@ -10,4 +10,11 @@ func main() {
 	var n bool = false
 	fmt.Printf("%v, %T\n", n, n)
 
+	m := 1 == 1
+	fmt.Printf("%v, %T\n", m, m)
+
+	//default value is always 0. in bool it is "false"
+	var k bool
+	fmt.Printf("%v, %T\n", k, k)
+
 }
